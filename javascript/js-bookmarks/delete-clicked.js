5947ca6fcbf4d8482898ed5419f21c67a9d700ef
Matt Billard <2836258+mattbillard@users.noreply.github.com>
2020-09-09 22:01:12 -0400

    Update delete-clicked JS bookmark

--- javascript/js-bookmarks/delete-clicked.js
+++ javascript/js-bookmarks/delete-clicked.js
@@ -1,7 +1,8 @@
-(() => { 
-  const deleteClicked = event => { 
+(() => {
+  const deleteClicked = event => {
     event.target.remove();
     event.stopPropagation();
+    event.preventDefault();
   };
   document.addEventListener('click', deleteClicked, true);
   setTimeout(() => document.removeEventListener('click', deleteClicked, true), 5000);
