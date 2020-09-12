658bdb07275fcd72d1de9a0bf76da2b37ec54119
Matt Billard <2836258+mattbillard@users.noreply.github.com>
2020-09-12 15:05:19 -0400

    Update delete-clicked

--- javascript/js-bookmarks/delete-clicked.js
+++ javascript/js-bookmarks/delete-clicked.js
@@ -4,6 +4,16 @@
     event.stopPropagation();
     event.preventDefault();
   };
-  document.addEventListener('click', deleteClicked, true);
-  setTimeout(() => document.removeEventListener('click', deleteClicked, true), 5000);
+  const addCss = () => {
+    document.addEventListener('click', deleteClicked, true);
+    var el = document.createElement('style');
+    el.id = 'temp123';
+    el.innerText = '*:hover { outline: solid 1px red !important;  }';
+    document.head.appendChild(el);
+  };
+  setTimeout(() => {
+    document.getElementById('temp123').remove();
+    document.removeEventListener('click', deleteClicked, true)
+  }, 5000);
+  addCss();
 })()
